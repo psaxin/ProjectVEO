1cc46123640d62b1209b97ec2f3837b667dfbca0
psaxin <pjar_Saxin@hotmail.com>
2016-08-22 20:28:09 +0200

    booting..

---
+++ ProjectVEO/ProjectVEO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectVEO.MvcApplication" Language="C#" %>
---
+++ ProjectVEO/ProjectVEO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectVEO.Startup))]
+namespace ProjectVEO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
